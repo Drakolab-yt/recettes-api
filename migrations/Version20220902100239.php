7ff473d03b677d3c7c5d2ddd8500118383db569e
Rémi Jarjat <remi@drakona.fr>
2022-09-02 12:39:19 +0200

    6 - Description de migration manquante

--- migrations/Version20220902100239.php
+++ migrations/Version20220902100239.php
@@ -14,7 +14,7 @@ final class Version20220902100239 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return '6 - Ajout de la relation entre Tag et Recipe (ManyToMany)';
     }
 
     public function up(Schema $schema): void
