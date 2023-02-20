e32a0b26acecd4afe09f2f33ab7d1b20b093dd91
Rémi Jarjat <remi@drakona.fr>
2023-02-20 15:24:23 +0100

    8 - Installation de LexikJWTAuthentifactionBundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
